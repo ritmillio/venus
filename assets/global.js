2295221a710ca10119c5b57b5301c7dad9ce1919
ritmillio <f.zoltan.dev@gmail.com>
2022-01-22 16:39:29 +0100

    Removing classes from main.css and turning them into TailwindCSS

--- assets/global.js
+++ assets/global.js
@@ -825,7 +825,7 @@ class VariantSelects extends HTMLElement {
 
         const price = document.getElementById(`price-${this.dataset.section}`);
 
-        if (price) price.classList.remove('visibility-hidden');
+        if (price) price.classList.remove('invisible');
         this.toggleAddButton(!this.currentVariant.available, window.variantStrings.soldOut);
       });
   }
@@ -856,7 +856,7 @@ class VariantSelects extends HTMLElement {
     const price = document.getElementById(`price-${this.dataset.section}`);
     if (!addButton) return;
     addButtonText.textContent = window.variantStrings.unavailable;
-    if (price) price.classList.add('visibility-hidden');
+    if (price) price.classList.add('invisible');
   }
 
   getVariantData() {
